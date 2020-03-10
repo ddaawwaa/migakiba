8fa37a02a0c6e7b7d4402c7ef10114f2c2ce546a
ddaawwaa <nlghtnmnt3106@gmail.com>
2020-03-10 19:15:52 +0900

    Create spots table
    - spotsテーブルを作成した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_150111) do
+ActiveRecord::Schema.define(version: 2020_03_10_090529) do
+
+  create_table "spots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.string "address", null: false
+    t.time "open_time"
+    t.time "close_time"
+    t.string "homepage"
+    t.integer "fauset_num", null: false
+    t.integer "towel", null: false
+    t.integer "avairable_user", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_spots_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
