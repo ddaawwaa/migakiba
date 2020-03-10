94224cc83067a1cbc9e139e18beaa5cf09884a09
ddaawwaa <nlghtnmnt3106@gmail.com>
2020-03-10 19:36:00 +0900

    Setting enum to some columns
    spotテーブルの以下のカラムにenumを設定
    - faucet_num
    - towel
    - avairable_user

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,6 @@
 class Spot < ApplicationRecord
+
+  enum faucet_num: { none: 0, one: 1, two: 2, thiree: 3, four_or_more: 4}
+  enum towel: {none: 0, air_towel: 1, paper_towel: 2}
+  enum avairable_user: {anyone: 0, only_concerned: 1}
 end
