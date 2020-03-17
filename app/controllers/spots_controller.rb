58c1773dbb03c377bd8f190ea6baa912aec099b3
ddaawwaa <nlghtnmnt3106@gmail.com>
2020-03-17 22:18:51 +0900

    Create view spots#new

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -2,4 +2,8 @@ class SpotsController < ApplicationController
   def index
     render "spots/index"
   end
+
+  def new
+    @spot = Spot.new
+  end
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,7 +1,7 @@
 class Spot < ApplicationRecord
   belongs_to :user
 
-  enum faucet_num: { none: 0, one: 1, two: 2, thiree: 3, four_or_more: 4}
-  enum towel: {none: 0, air_towel: 1, paper_towel: 2}
+  enum faucet_num: { no_faucet: 0, one: 1, two: 2, three: 3, four_or_more: 4}
+  enum towel: {no_towel: 0, air_towel: 1, paper_towel: 2}
   enum avairable_user: {anyone: 0, only_concerned: 1}
 end
