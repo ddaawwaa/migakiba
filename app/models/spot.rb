3aaf94ca209c60373717d6d3a10643c23f426575
ddaawwaa <nlghtnmnt3106@gmail.com>
2020-03-29 16:31:16 +0900

    Add google map to spots#new

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,6 +3,9 @@ class Spot < ApplicationRecord
   has_many :photos
   accepts_nested_attributes_for :photos
 
+  geocoded_by :address
+  after_validation :geocode
+
   enum faucet_num: { no_faucet: 0, one: 1, two: 2, three: 3, four_or_more: 4}
   enum towel: {no_towel: 0, air_towel: 1, paper_towel: 2}
   enum avairable_user: {anyone: 0, only_concerned: 1}
